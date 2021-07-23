ec02a0536f4620bea90d4536326c81af18844f83
Elisabeth Kittle <elisabethkittle@gmail.com>
2021-07-23 18:20:28 -0400

    Initial commit following create-react-app

---
+++ src/components/BlogHome.js
@@ -0,0 +1,101 @@
+
+
+const posts = [
+    {
+        "_id": "60fb091f8083fd16baa1496a",
+        "title": "Post1",
+        "content": "Lots of content for post 1",
+        "date": "2021-07-23T04:00:00.000Z",
+        "published": true,
+        "__v": 0
+    },
+    {
+        "_id": "60fb091f8083fd16baa1496d",
+        "title": "Post2",
+        "content": "Lots of content for post 2",
+        "date": "2021-07-23T04:00:00.000Z",
+        "published": true,
+        "__v": 0
+    },
+    {
+        "_id": "60fb091f8083fd16baa1496f",
+        "title": "Post3",
+        "content": "Lots of content for post 3",
+        "date": "2021-07-23T04:00:00.000Z",
+        "published": true,
+        "__v": 0
+    },
+    {
+        "_id": "60fb091f8083fd16baa14971",
+        "title": "Post4",
+        "content": "Lots of content for post 4",
+        "date": "2021-07-23T04:00:00.000Z",
+        "published": true,
+        "__v": 0
+    },
+    {
+        "_id": "60fb091f8083fd16baa14973",
+        "title": "Post5",
+        "content": "Lots of content for post 5",
+        "date": "2021-07-23T04:00:00.000Z",
+        "published": true,
+        "__v": 0
+    }
+];
+
+const comments = [
+    {
+        "name": "Guest",
+        "_id": "60fb09208083fd16baa14981",
+        "comment": "A harsh comment",
+        "date": "2021-07-23T04:00:00.000Z",
+        "post": "60fb091f8083fd16baa1496a",
+        "__v": 0
+    },
+    {
+        "name": "Guest",
+        "_id": "60fb09208083fd16baa14975",
+        "comment": "A harsh comment",
+        "date": "2021-07-23T04:00:00.000Z",
+        "post": "60fb091f8083fd16baa1496d",
+        "__v": 0
+    },
+    {
+        "name": "Guest",
+        "_id": "60fb09208083fd16baa14977",
+        "comment": "A nice comment",
+        "date": "2021-07-23T04:00:00.000Z",
+        "post": "60fb091f8083fd16baa1496d",
+        "__v": 0
+    },        {
+        "name": "Guest",
+        "_id": "60fb09208083fd16baa14979",
+        "comment": "A harsh comment",
+        "date": "2021-07-23T04:00:00.000Z",
+        "post": "60fb091f8083fd16baa1496f",
+        "__v": 0
+    },
+    {
+        "name": "Guest",
+        "_id": "60fb09208083fd16baa1497b",
+        "comment": "A brutal comment",
+        "date": "2021-07-23T04:00:00.000Z",
+        "post": "60fb091f8083fd16baa1496f",
+        "__v": 0
+    },        {
+        "name": "Guest",
+        "_id": "60fb09208083fd16baa1497d",
+        "comment": "A harsh comment",
+        "date": "2021-07-23T04:00:00.000Z",
+        "post": "60fb091f8083fd16baa14971",
+        "__v": 0
+    },
+    {
+        "name": "Guest",
+        "_id": "60fb09208083fd16baa1497f",
+        "comment": "A exhausted comment",
+        "date": "2021-07-23T04:00:00.000Z",
+        "post": "60fb091f8083fd16baa14971",
+        "__v": 0
+    }
+]
